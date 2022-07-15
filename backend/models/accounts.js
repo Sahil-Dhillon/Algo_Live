8aba424cb272cdacff74cd790e26a2770dd6d132
HabeebUllahTabrez <ullahabeeb11@gmail.com>
2022-07-15 18:37:33 +0530

    changed account schema

--- backend/models/accounts.js
+++ backend/models/accounts.js
@@ -7,39 +7,31 @@ var AccountSchema = new mongoose.Schema({
         ref: "User",
         required: true,
     },
-    userID: {
-        type: String,
-        required: true,
-        unique: true,
-    },
     broker: {
         type: String,
         required: true,
     },
-    name: {
+    userID: {
         type: String,
         required: true,
+        unique: true,
     },
     password: {
         type: String,
         required: true,
     },
     pin: String,
-    
     auth_type: String,
     totp_secret: String,
     apiKey: String,
     secret: String,
     accessToken: String, // not to be shoqn
     enctoken: String, //
-   
     balance: {
-        //
         type: Number,
         required: true,
         default: 0,
     },
-    
 },
     { timestamps: true }
 );
